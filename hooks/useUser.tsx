aec03cd0c2831c905cd3bfc73c02437b09424eef
Sandeep <sandeepiitm2003@gmail.com>
2023-07-08 10:50:47 +0530

    added a modal

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,9 +1,11 @@
 
+"use client"
+
 import { Subscription, UserDetails } from "@/types";
 
 import {useSessionContext, useUser as useSupaUser} from "@supabase/auth-helpers-react";
 import {User} from "@supabase/auth-helpers-nextjs" 
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 
 type UserContextType = {
@@ -74,8 +76,17 @@ supabaseClient: supabase,
         subscription
 
     };
-    return <UserContext.Provider value = {value} {...props}/>
-    
+    return <UserContext.Provider value = {value} {...props}/> 
+};
+
+export const useUser = () =>{
+    const context = useContext(UserContext);
+    if(context === undefined){
+        throw new Error("useUser must be used within a MyUserContextProvider");
+
+    }
+    return context;
 
 }
+
 
