3d79b7674fe146c5ca1ca81800178ddb5dceda41
Sandeep <sandeepiitm2003@gmail.com>
2023-07-10 13:15:07 +0530

    final commit

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,5 +1,6 @@
 "use client"
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
@@ -16,13 +17,14 @@ const MediaItem:React.FC<MediaItemProps> = ({
     onClick,
     className
 }) =>{
+    const player = usePlayer();
     const imageUrl = useLoadImage(data);
     const handleClick = ()=>{
         if(onClick){
             return onClick(data.id)
 
         }
-        //default turn on player
+        return player.setId(data.id);
 
 
     }
