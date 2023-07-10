d12beb175071c16f1aec701745104e3727dfb4c7
Sandeep <sandeepiitm2003@gmail.com>
2023-07-10 14:33:04 +0530

    djsl

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,42 +1,38 @@
-"use client"
-import queryString from 'query-string';
+"use client";
 
-
-import qs from "query-string"
-import useDebounce from "@/hooks/useDebounce";
-import { useRouter } from "next/navigation";
+import qs from "query-string";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import useDebounce from "@/hooks/useDebounce";
+
 import Input from "./Input";
 
-const SearchInput = () =>{
-    const router = useRouter();
-    const [value, setValue] = useState<string>("");
-    const debouncedValue = useDebounce<string>(value, 500);
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(value, 500);
 
-    useEffect(()=>{
-        const query = {
-            title: debouncedValue,
-        
-        };
-        const url = qs.stringifyUrl({
-            url:'/search',
-            query: query
-        })
-        router.push(url)
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
 
-    }, [debouncedValue, router])
+    const url = qs.stringifyUrl({
+      url: '/search',
+      query
+    });
 
+    router.push(url);
+  }, [debouncedValue, router]);
 
-    return (
-       <Input
-            placeholder="what do you want to listen :)"
-            value={value}
-            onChange={(e)=>{
-                setValue(e.target.value)
-            }}
-       />
-    )
-
+  return ( 
+    <Input 
+      placeholder="What do you want to listen to?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 }
-
+ 
 export default SearchInput;
